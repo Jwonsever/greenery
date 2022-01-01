77aec33685a69325c51cefa78a12d736101342e7
James Wonsever <james.wonsever@alphaflow.com>
2021-12-31 17:31:00 -0800

    Slow day.  Practice crawling.

--- committed.sh
+++ committed.sh
@@ -3,6 +3,6 @@ NUMBER=$(($RANDOM % 7))
 
 for ((run = 1; run <= NUMBER + 1; run++)); do
   echo $(/usr/local/bin/fortune) >/Users/jwonsever/dev_personal/greenery/file.txt
-  /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ commit -am "$(fortune -sn 32)"
+  /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ commit -am "$(/usr/local/bin/fortune -sn 32)"
   /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ push
 done
