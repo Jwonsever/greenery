694a4126ab9c88d3bf1bd5e8d0cc1a4fa6023da6
James Wonsever <james.wonsever@alphaflow.com>
2021-12-31 17:01:20 -0800

    Now I am depressed ...

--- committed.sh
+++ committed.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 NUMBER=$(($RANDOM % 7))
 
-for ((run=1; run <= NUMBER + 1; run++))
-do
-  echo `fortune` > ~/dev_personal/greenery/file.txt
-  /usr/bin/git -C ~/dev_personal/greenery/ add . -A
-  /usr/bin/git -C ~/dev_personal/greenery/ commit -m "`fortune -sn 32`"
-  /usr/bin/git -C ~/dev_personal/greenery/ push
+for ((run = 1; run <= NUMBER + 1; run++)); do
+  echo $(fortune) >/Users/jwonsever/dev_personal/greenery/file.txt
+  /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ add . -A
+  /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ commit -m "$(fortune -sn 32)"
+  /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ push
 done
-
