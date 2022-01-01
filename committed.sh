c5d0443aee9ee57fd42a357bfb85f5896fb16551
James Wonsever <james.wonsever@alphaflow.com>
2021-12-31 17:04:13 -0800

    simplify

--- committed.sh
+++ committed.sh
@@ -3,7 +3,6 @@ NUMBER=$(($RANDOM % 7))
 
 for ((run = 1; run <= NUMBER + 1; run++)); do
   echo $(fortune) >/Users/jwonsever/dev_personal/greenery/file.txt
-  /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ add . -A
-  /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ commit -m "$(fortune -sn 32)"
+  /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ commit -am "$(fortune -sn 32)"
   /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ push
 done
