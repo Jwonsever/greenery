655d33f721efe87f33b2b8125710c453bee4571e
James Wonsever <james.wonsever@alphaflow.com>
2021-12-31 17:17:43 -0800

    test

--- committed.sh
+++ committed.sh
@@ -3,6 +3,7 @@ NUMBER=$(($RANDOM % 7))
 
 for ((run = 1; run <= NUMBER + 1; run++)); do
   echo $(fortune) >/Users/jwonsever/dev_personal/greenery/file.txt
+  echo 'test' >>/Users/jwonsever/dev_personal/greenery/file.txt
   /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ commit -am "$(fortune -sn 32)"
   /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ push
 done
