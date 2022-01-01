02e743920b47168b514c90e3f41b1756b3690751
James Wonsever <james.wonsever@alphaflow.com>
2021-12-31 17:22:13 -0800

    fix calls

--- committed.sh
+++ committed.sh
@@ -2,7 +2,7 @@
 NUMBER=$(($RANDOM % 7))
 
 for ((run = 1; run <= NUMBER + 1; run++)); do
-  echo $(fortune) >/Users/jwonsever/dev_personal/greenery/file.txt
+  echo $(/usr/local/bin/fortune) >/Users/jwonsever/dev_personal/greenery/file.txt
   echo 'test' >>/Users/jwonsever/dev_personal/greenery/file.txt
   /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ commit -am "$(fortune -sn 32)"
   /usr/bin/git -C /Users/jwonsever/dev_personal/greenery/ push
